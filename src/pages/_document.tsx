738a4037de28bffed9e19e78101e945c36edd523
Wendler Eis <wendler_eis@hotmail.com>
2021-03-02 23:09:16 -0300

    Add material-ui (#6)
    * feat: setup server side render styles with mui and styled components
    
    * feat: configure mui's tree-shaking

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,31 +1,34 @@
 import Document, { DocumentContext, DocumentInitialProps } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
+import { ServerStyleSheet as StyledComponentsSheets } from 'styled-components'
+import { ServerStyleSheets as MaterialUiSheets } from '@material-ui/core/styles'
 export default class MyDocument extends Document {
-  static async getInitialProps(
-    ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
-    const sheet = new ServerStyleSheet()
+  static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+    const styledComponentSheets = new StyledComponentsSheets()
+    const materialUiSheets = new MaterialUiSheets()
+
     const originalRenderPage = ctx.renderPage
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+          enhanceApp: App => props => styledComponentSheets.collectStyles(materialUiSheets.collect(<App {...props} />)),
         })
 
       const initialProps = await Document.getInitialProps(ctx)
+
       return {
         ...initialProps,
-        styles: (
+        styles: [
           <>
             {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
+            {materialUiSheets.getStyleElement()}
+            {styledComponentSheets.getStyleElement()}
+          </>,
+        ],
       }
     } finally {
-      sheet.seal()
+      styledComponentSheets.seal()
     }
   }
 }
