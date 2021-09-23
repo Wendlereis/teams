337a586808adbf2d01bd55c5fb8126c05fe50e8b
Wendler Eis <wendler_eis@hotmail.com>
2021-09-22 21:38:14 -0300

    feat: upgrade theme types

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -5,79 +5,83 @@ import { ThemeProvider as EmotionProvider } from '@emotion/react'
 import { ThemeProvider as MuiTheme, createTheme, adaptV4Theme } from '@mui/material'
 
 export const ThemeProvider: FC = ({ children }) => {
-  const theme = createTheme(
-    adaptV4Theme({
-      typography: {
-        fontFamily: 'Lato',
-        h1: {
-          fontSize: '48px',
-          lineHeight: '52px',
-        },
-        h2: {
-          fontSize: '40px',
-          lineHeight: '48px',
-        },
-        h3: {
-          fontSize: '32px',
-          lineHeight: '40px',
-        },
-        h4: {
-          fontSize: '24px',
-          lineHeight: '32px',
-        },
-        h5: {
-          fontSize: '20px',
-          lineHeight: '28px',
-        },
-        body1: {
-          fontSize: '16px',
-          lineHeight: '24px',
-        },
-        body2: {
-          fontSize: '14px',
-          lineHeight: '20px',
-        },
-        button: {
-          fontSize: '16px',
-          lineHeight: '24px',
-          textTransform: 'capitalize',
-        },
+  const theme = createTheme({
+    typography: {
+      fontFamily: 'Lato',
+      h1: {
+        fontSize: '48px',
+        lineHeight: '52px',
       },
-      palette: {
-        primary: {
-          main: '#1b4965',
-        },
-        secondary: {
-          main: '#5fa8d3',
-        },
-        background: {
-          default: '#f2f2f2',
-          paper: '#ffffff',
-        },
-        text: {
-          primary: '#00000085',
-          secondary: '#00000060',
-        },
+      h2: {
+        fontSize: '40px',
+        lineHeight: '48px',
       },
-      overrides: {
-        MuiButton: {
+      h3: {
+        fontSize: '32px',
+        lineHeight: '40px',
+      },
+      h4: {
+        fontSize: '24px',
+        lineHeight: '32px',
+      },
+      h5: {
+        fontSize: '20px',
+        lineHeight: '28px',
+      },
+      body1: {
+        fontSize: '16px',
+        lineHeight: '24px',
+      },
+      body2: {
+        fontSize: '14px',
+        lineHeight: '20px',
+      },
+      button: {
+        fontSize: '16px',
+        lineHeight: '24px',
+        textTransform: 'capitalize',
+      },
+    },
+    palette: {
+      primary: {
+        main: '#1b4965',
+      },
+      secondary: {
+        main: '#5fa8d3',
+      },
+      background: {
+        default: '#f2f2f2',
+        paper: '#ffffff',
+      },
+      text: {
+        primary: '#00000085',
+        secondary: '#00000060',
+      },
+    },
+    components: {
+      MuiButton: {
+        styleOverrides: {
           root: {
             borderRadius: '8px',
           },
         },
-        MuiOutlinedInput: {
+      },
+      MuiOutlinedInput: {
+        styleOverrides: {
           root: {
             borderRadius: '8px',
           },
         },
-        MuiPaper: {
+      },
+      MuiPaper: {
+        styleOverrides: {
           rounded: {
             borderRadius: '16px',
           },
         },
       },
-    })
-  )
+    },
+  })
 
   return (
     <MuiTheme theme={theme}>
