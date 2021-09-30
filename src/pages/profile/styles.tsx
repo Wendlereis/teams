9f25d3af9b41a82022803084cf97d399a96b2644
Wendler Eis <wendler_eis@hotmail.com>
2021-09-30 20:59:16 -0300

    :sparkles: feat: add styles on Profile page

---
+++ src/pages/profile/styles.tsx
@@ -0,0 +1,41 @@
+import styled from '@emotion/styled'
+
+import { Paper as MuiPaper, Typography as MuiTypography } from '@mui/material'
+
+export const Wrapper = styled.section`
+  ${({ theme: { breakpoints } }) => `
+    margin-top: 32px;
+
+    ${breakpoints.up('md')} {
+      margin-top: 56px;
+    }
+  `}
+`
+
+export const TitleWraper = styled.section`
+  margin-bottom: 24px;
+`
+
+export const Form = styled.form`
+  margin-top: 32px;
+`
+
+export const BoardWrapper = styled.section`
+  margin-bottom: 24px;
+`
+
+export const Board = styled(MuiPaper)`
+  padding: 24px 16px 16px 16px;
+`
+
+export const BoardTitle = styled(MuiTypography)`
+  margin-bottom: 16px;
+`
+
+export const ButtonWrapper = styled.section`
+  display: flex;
+  justify-content: flex-end;
+
+  margin-top: 32px;
+  margin-bottom: 80px;
+`
--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -50,7 +50,7 @@ export const ThemeProvider: FC = ({ children }) => {
         main: '#5fa8d3',
       },
       background: {
-        default: '#f7f7f7',
+        default: '#f9fbfd',
         paper: '#ffffff',
       },
       text: {
@@ -62,7 +62,7 @@ export const ThemeProvider: FC = ({ children }) => {
       MuiAppBar: {
         styleOverrides: {
           colorDefault: {
-            backgroundColor: '#f7f7f7',
+            backgroundColor: '#ffffff',
           },
         },
       },
@@ -81,9 +81,13 @@ export const ThemeProvider: FC = ({ children }) => {
         },
       },
       MuiPaper: {
+        defaultProps: {
+          elevation: 0,
+          variant: 'outlined',
+        },
         styleOverrides: {
           rounded: {
-            borderRadius: '16px',
+            borderRadius: '8px',
           },
         },
       },
@@ -101,6 +105,11 @@ export const ThemeProvider: FC = ({ children }) => {
             minWidth: '0px',
             marginRight: '16px',
           },
+        },
+      },
+      MuiContainer: {
+        defaultProps: {
+          maxWidth: 'md',
         },
       },
     },
