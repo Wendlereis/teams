896364dd3d64468d594d06d216c74f8138a9aa0b
Wendler Eis <wendler_eis@hotmail.com>
2021-10-07 21:43:28 -0300

    :sparkles: feat: add PageTitle component

---
+++ src/components/PageTitle/PageTitle.tsx
@@ -0,0 +1,19 @@
+import { Typography } from '@mui/material'
+
+import { Props } from './types'
+
+import * as S from './styles'
+
+function PageTitle({ title, subtitle }: Props): JSX.Element {
+  return (
+    <S.Wraper>
+      <Typography variant="h2">{title}</Typography>
+
+      <Typography variant="subtitle1" color="text.secondary">
+        {subtitle}
+      </Typography>
+    </S.Wraper>
+  )
+}
+
+export default PageTitle
